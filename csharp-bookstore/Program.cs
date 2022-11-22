17d1efa6223c72211ce3f065a956eff0d7d4ecae
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-22 14:50:09 +0100

    Creato file + inserimento prime variabili

--- csharp-bookstore/Program.cs
+++ csharp-bookstore/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string titolo = "La guerra dei mondi";
+string autore = "H. G. Wells";
+string
